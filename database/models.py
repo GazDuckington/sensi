9c77d28fe2d8c78eb34e832f11ee43116db68e63
Ghazy <dianghazy@protonmail.com>
2022-08-26 17:42:14 +0700

    up: sqla logging=off

--- database/models.py
+++ database/models.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 conn = "sqlite:///" + os.path.join(BASE_DIR, "dataset.db") + "?check_same_thread=False"
-engine = create_engine(conn, echo=True)
+engine = create_engine(conn)
 Base = declarative_base()
 session = sessionmaker()
 
